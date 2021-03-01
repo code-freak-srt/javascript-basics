b46129d4b2932ff9d5ffc12e1a96e907b514945d
Sushil <sushilrajthala421@gmail.com>
2021-03-01 09:44:43 +0545

    Learning .....

--- vuejs/index.js
+++ vuejs/index.js
@@ -1,19 +1,44 @@
 // creating vue Instance
+// var app = new Vue({
+//     el: '#app',
+
+//     data: {
+//         message: "Vue is fun",
+//         name: "Sushil",
+//         price: '1000',
+//         path: 'http://wallpapersdsc.net/wp-content/uploads/2016/10/River-Images.jpg',
+//         link: 'http://www.wikipedia.com',
+//     },
+// creating Vue Methods
+//     methods: {
+//         sellingPrice: function(d) {
+//             cost = this.price - d;
+//             return cost;
+//         }
+//     }
+
+
+// })
+
 var app = new Vue({
     el: '#app',
 
     data: {
-        message: "Vue is fun",
-        name: "Sushil",
-        price: '1000',
+        temp: 0,
+        message: '',
+        status: true,
+        gate: true,
+
     },
-    // creating Vue Methods
     methods: {
-        sellingPrice: function(d) {
-            cost = this.price - d;
-            return cost;
+        inc: function() {
+            this.temp++;
+        },
+        dec: function() {
+            this.temp--;
+        },
+        keypressed: function() {
+            console.log('key pressed');
         }
     }
-
-
 })
